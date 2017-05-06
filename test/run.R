1ba278f4a150ff949ebb016f00eccf612e8f3a4a
jmzeng1314 <jmzeng1314@163.com>
2017-05-06 23:45:07 +0800

    create rd files by roxygen2

--- test/run.R
+++ test/run.R
@@ -1,13 +1,23 @@
-input_file='/Users/jimmy/Dropbox/processing-program/sustc-iris/vdjTools/2.segments.wt.V.txt'
+input_file='test/input_files/V-usage-all-samples.txt'
 df<-read.table(input_file, header=T, sep="\t", comment="")
 head(df)
 vcols<-4:51
 df[, vcols] <- apply(df[, vcols], 2, as.numeric)
 x <- as.matrix(df[, vcols])
 rownames(x)=df[,1]
-vdj_usage_ht(x,'v-usage-heatmap.pdf')
+vdj_usage_ht(x,'test/input_files/v-usage-heatmap.pdf')
 
-file_in='/Users/jimmy/Dropbox/processing-program/sustc-iris/vdjTools/5.fancyvj.wt.txt'
+input_file='test/input_files/J-usage-all-samples.txt'
+df<-read.table(input_file, header=T, sep="\t", comment="")
+head(df)
+vcols<-4:16
+df[, vcols] <- apply(df[, vcols], 2, as.numeric)
+x <- as.matrix(df[, vcols])
+rownames(x)=df[,1]
+vdj_usage_ht(x,'test/input_files/j-usage-heatmap.pdf')
+
+
+file_in='test/input_files/V-J-usage-for-a-sample.txt'
 temp <- read.table(file_in, sep="\t", comment="")
 n <- nrow(temp)
 m <- ncol(temp)
@@ -16,6 +26,6 @@ cn = apply(temp[1,2:m], 2 , as.character)
 mat <- matrix(apply(temp[2:n, 2:m], 1:2, as.numeric), n - 1, m-1)
 rownames(mat)=rn
 colnames(mat)=cn
-v_j_circle(mat,'v-j-circle.pdf')
+v_j_circle(mat,'test/input_files/v-j-circle.pdf')
 
 
